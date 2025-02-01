2598dcb39eb1c76b534b9cd502b576147b776ab8
satyakisetu <satyakisetu@gmail.com>
2025-02-01 15:29:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Satyaki",
+    author_email="satyakisetu@gmail.com",
+    packages=find_packages()
+)
